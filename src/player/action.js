e8b009e3ad88bbc898ef89c766030fb82de06461
David Jegat <david.jegat@knplabs.com>
2017-06-22 15:26:44 +0200

    Wip

--- src/player/action.js
+++ src/player/action.js
@@ -4,8 +4,9 @@ export function getAction(DOM) {
     const keyDown$ = DOM.select('document').events('keydown');
     const keyLeft$ = keyDown$.filter(ev => ev.keyCode === 37).mapTo(-1).startWith(0);
     const keyRight$ = keyDown$.filter(ev => ev.keyCode === 39).mapTo(1).startWith(0);
+    const space$ = keyDown$.filter(ev => ev.keyCode === 32).mapTo('stick').startWith(0);
 
-    return xs.merge(keyLeft$, keyRight$)
+    return xs.merge(keyLeft$, keyRight$, space$)
         .map(direction => xs.fromArray([direction, 0]))
         .flatten()
     ;
