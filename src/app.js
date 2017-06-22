522fae6c0d967f3abc6129751ead9aa10a14c4f3
Gildas Quéméner <gildas.quemener@gmail.com>
2017-06-22 12:12:45 +0200

    Implemented action stream

--- src/app.js
+++ src/app.js
@@ -3,18 +3,30 @@ import {div} from '@cycle/dom'
 import xs from 'xstream'
 
 export function App ({DOM, Time, Canvas}) {
-    const frames$ = Time.animationFrames();
+    const frame$ = Time.animationFrames();
+    const keyDown$ = DOM.select('document').events('keydown');
+    const keyUp$ = DOM.select('document').events('keyup').mapTo(0).startWith(0);
+    const keyLeft$ = keyDown$.filter(ev => ev.keyCode === 37).mapTo(-1).startWith(0);
+    const keyRight$ = keyDown$.filter(ev => ev.keyCode === 39).mapTo(1).startWith(0);
+
+
+    const action$ = xs.merge(keyLeft$, keyRight$, keyUp$);
 
     return {
-        //Canvas: frames$.map(drawCanvas),
-        DOM: frames$.map(drawDOM),
+        Canvas: xs.combine(frame$, action$)
+            .map(drawCanvas),
     }
 }
 
-function drawCanvas(f) {
-    return rect({draw: [{fill: 'skyblue'}]}, [
-        text({x: 10, y: 10, value: `frame: ${f.delta}`}),
-    ]);
+function drawCanvas(stream) {
+        stream.fold((acc, [frame, action]) => {
+                acc.x = acc.x + action;
+
+                return acc;
+            }, { x: 10, y: 10 })
+            .map((position) => rect({draw: [{fill: 'skyblue'}]}, [
+                text({x: position.x, y: position.y, value: `frame: salut`}),
+            ]))
 }
 
 function drawDOM(f) {
