882ae7b1657e1d9ce26c322c6a533bd80a2d5ab9
schevalier <chevalier.syl20@gmail.com>
2017-06-22 16:30:45 +0200

    Added enemy moves limitation

--- src/drawer/dom.js
+++ src/drawer/dom.js
@@ -2,13 +2,12 @@ import {div} from '@cycle/dom'
 
 import {foldState} from '../state'
 
-<<<<<<< HEAD
 export function draw(game$) {
     return game$
         .compose(foldState)
-        .map((position) => div([
-            div('#player.position-' + position.player),
-            div('#enemy', { style: { left: position.enemy + 'px' }}),
+        .map((state) => div([
+            div('#player.position-' + state.position),
+            div('#enemy.position-' + state.enemy),
             ...state.obstacles.map(obstacle => div(`.obstacle.position-${obstacle.x}`))
         ]))
 }
